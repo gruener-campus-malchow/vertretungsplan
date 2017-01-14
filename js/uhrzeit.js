a91b1c3656fc4daad968e6d50de8d51c5dd84d54
nicolas2307 <raubenicolas@gmail.com>
2017-01-14 02:43:58 +0100

    merge

---
+++ js/uhrzeit.js
@@ -0,0 +1,10 @@
+
+
+function updateTime() {
+    var hours_div = document.getElementById("hours");
+    var minutes_div = document.getElementById("minutes");
+    var now = new Date();
+    hours_div.innerText = now.getHours();
+    minutes_div.innerText = now.getMinutes();
+}
+window.setInterval(updateTime, 1000);
