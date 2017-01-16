1d3271f2e663dab741f0eb4b02264548a544a5aa
nicolas2307 <raubenicolas@gmail.com>
2017-01-16 19:08:10 +0100

    time displaying as 01, 02, ... instead of 1,2,...

--- js/uhrzeit.js
+++ js/uhrzeit.js
@@ -3,8 +3,20 @@ window.onload=updateTime;
 function updateTime() {
     var hours_div = document.getElementById("hours");
     var minutes_div = document.getElementById("minutes");
+
     var now = new Date();
-    hours_div.innerText = now.getHours();
-    minutes_div.innerText = now.getMinutes();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+
+    if(hours < 10) {
+      hours_div.innerText = '0' + hours;
+    } else {
+    }
+
+    if(minutes < 10) {
+      minutes_div.innerText = '0' + minutes;
+    } else {
+    }
+
 }
 window.setInterval(updateTime, 1000);
