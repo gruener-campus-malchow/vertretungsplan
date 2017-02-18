d8aca88f669c1f9a9bda04e6c4e8015e4322778b
nicolasraube <raubenicolas@gmail.com>
2017-02-18 12:18:59 +0100

    auto scroll

---
+++ js/scroll.js
@@ -0,0 +1,46 @@
+const SCROLL_DELAY = 3000;
+var scrollDistance;
+var intervalScroll;
+
+function startScrolling() {
+  intervalScroll = setInterval(scroll, SCROLL_DELAY);
+}
+
+function scroll() {
+  calcScrollDistance();
+
+  if(bottomReached()) {
+    scrollToTop();
+  } else {
+    scrollDown();
+  }
+}
+
+//animated scrolling source: https://stackoverflow.com/questions/14817874/animated-javascript-scrollby
+function scrollToTop() {
+  $('html,body').stop().animate({
+    scrollTop: 0
+  });
+}
+
+function scrollDown() {
+  $('html,body').stop().animate({
+    scrollTop: '+=' + scrollDistance
+  });
+}
+
+function bottomReached() {
+  return $(document).height()===getOffsetTop();
+}
+
+function calcScrollDistance() {
+  scrollDistance = $(window).height() - getHeaderHeight() - 50;
+}
+
+function getOffsetTop() {
+  return $(document).scrollTop() + $(window).height();
+}
+
+function stopScrolling() {
+  clearInterval(intervalScroll);
+}
