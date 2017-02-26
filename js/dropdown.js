538163d16d45744e478609795cef294cacdad21c
nicolasraube <raubenicolas@gmail.com>
2017-02-26 09:59:06 +0100

    sorting dropdown options, added function sortClassNames()

--- js/dropdown.js
+++ js/dropdown.js
@@ -11,17 +11,35 @@ function dropdownChanged() {
   redirect(urlPlanhtml + getUrlArguments(), 0);
 }
 
-function addClassNamesToDropdown() {
-  for (var i = 0; i < allClassNames.length; i++) {
-    var className = allClassNames[i];
+function addClassNamesToDropdown(classNames) {
+  for (var i = 0; i < classNames.length; i++) {
+    var className = classNames[i];
     var option = new Option();
     option.text = className;
     getDropdown().options.add(option);
   }
 
-  if (allClassNames.includes(parameters['klasse'])) {
+  if (classNames.includes(parameters['klasse'])) {
     getDropdown().value=parameters['klasse'];
   }
+}
+
+function sortClassNames(classNames) {
+  var numberPattern = new RegExp("^\\d+");
+
+  classNames.sort(function (s1, s2) {
+      var ret = parseInt(numberPattern.exec(s1)) - parseInt(numberPattern.exec(s2));
+      if (ret != 0) {
+          return ret;
+      } else if (s1 < s2) {
+          return -1;
+      } else if (s1 > s2) {
+          return 1;
+      }
+      return 0;
+      });
+
+  return classNames;
 }
 
 function getDropdown() {
