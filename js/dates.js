7f905db29e732193132942ecc8e7b8f399b91e9d
nicolasraube <raubenicolas@gmail.com>
2017-09-19 17:41:58 +0200

    refactored updateTime() and time element

--- js/dates.js
+++ js/dates.js
@@ -13,25 +13,10 @@ var endTimes = [
   '16:15'
 ];
 
-var hours_div = document.getElementById("hours");
-var minutes_div = document.getElementById("minutes");
+var timeElement = document.getElementById("time");
 
 function updateTime() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-
-    if(hours < 10) {
-      hours_div.innerText = '0' + hours;
-    } else {
-      hours_div.innerText = hours;
-    }
-
-    if(minutes < 10) {
-      minutes_div.innerText = '0' + minutes;
-    } else {
-      minutes_div.innerText = minutes;
-    }
+    timeElement.innerText = getCurrentTime();
 }
 window.setInterval(updateTime, 1000);
 
