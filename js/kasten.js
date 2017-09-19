283508e4ee0c99c71e3b3e4239f770d45d8d5e5b
nicolasraube <raubenicolas@gmail.com>
2017-09-19 17:30:52 +0200

    working time comparison

--- js/kasten.js
+++ js/kasten.js
@@ -211,16 +211,42 @@ function isStundeInPast(date, stunde) {
   return false;
 }
 
-function getEndTimeInMinutes(stunde) {
+var endTimes = [
+  '8:00',
+  '8:55',
+  '9:40',
+  '10:45',
+  '11:40',
+  '12:40',
+  '13:30',
+  '14:35',
+  '15:25',
+  '16:15'
+];
 
+function getEndTimeInMinutes(stunde) {
+  return convertTimeToMinutes(endTimes[parseInt(stunde)]);
 }
 
 function getCurrentTime() {
+  var date = new Date();
 
+  var hours = date.getHours();
+  var mins = date.getMinutes();
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (mins < 10) {
+    mins = '0' + mins;
+  }
+
+  return hours + ':' + mins;
 }
 
 function convertTimeToMinutes(time) {
-  
+  time = time.split(':');
+  return parseInt(time[0]) * 60 + parseInt(time[1]);
 }
 
 function getCurrentDate() {//https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
