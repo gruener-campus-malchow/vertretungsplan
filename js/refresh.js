55b3d72c05c90a06eb7566f1d76b7ad407784b56
Maurice Rufnak <maurice-rufnak@memaxe.com>
2018-05-06 02:04:02 +0200

    Update #1 06.05.2018
    Many changes.

--- js/refresh.js
+++ js/refresh.js
@@ -1,6 +1,7 @@
 var user = parameters['user'];
 const REFRESH_DELAY = 5 * 60 * 1000;
 
-if (user === 'foyer') {
+if (user === 'foyer')
+{
   redirect(urlPlanhtml + getUrlArguments(), REFRESH_DELAY);
 }
